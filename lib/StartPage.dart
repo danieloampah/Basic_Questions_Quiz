7214eaa86bebf01ad2beb7cf5d80077565253366
musti1 <mstfhll353@gmail.com>
2022-12-01 14:48:37 +0300

    New Design Update

--- lib/StartPage.dart
+++ lib/StartPage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'questionsPage.dart';
 
 class StartPage extends StatefulWidget {
@@ -12,11 +11,11 @@ class StartPage extends StatefulWidget {
 class _StartPageState extends State<StartPage> {
   @override
   Widget build(BuildContext context) {
+    var screenInfo = MediaQuery.of(context);
+    final double screenHeight = screenInfo.size.height;
+    final double screenWidth = screenInfo.size.width;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromRGBO(28, 28, 28, 1),
-        leading: Icon(Icons.settings),
-      ),
+
       backgroundColor: Color.fromRGBO(28, 28, 28, 1),
       body: Center(
         child: Column(
@@ -26,68 +25,147 @@ class _StartPageState extends State<StartPage> {
               "Hoşgeldiniz",
               style: TextStyle(
                   color: Colors.white,
-                  fontSize: 20,
-                  ),
+                  fontSize: screenWidth / 23,
+                  fontWeight: FontWeight.bold),
             ),
             Padding(
-              padding: const EdgeInsets.only(top: 40, left: 20, right: 20),
+              padding: EdgeInsets.only(
+                  top: screenHeight / 25,
+                  left: screenWidth / 30,
+                  right: screenWidth / 30),
               child: Container(
-                width: 450,
-                height: 160,
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(30),
                 ),
-                child: Column(
-                  children: [
-                    Image.asset("assets/images/man.png"),
-                    SizedBox(height: 10,),
-                    Padding(
-                      padding: const EdgeInsets.all(13),
-                      child: Text("Angel Sara",style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
-                    ),
-                    Center(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Column(
-                            children: [
-                              Text("Balance", style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15),),
-                              Text("Rs.111", style: TextStyle(color: Colors.purple, fontWeight: FontWeight.bold, fontSize: 14),),
-                            ],
-                          ),
-                          SizedBox(
-                            width: 60,
-                          ),
-                          Column(
-                            children: [
-                              Text("Rank",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15),),
-                              Text("10",style: TextStyle(color: Colors.purple, fontWeight: FontWeight.bold, fontSize: 14),),
-                            ],
-                          ),
-                          SizedBox(
-                            width: 60,
-                          ),
-                          Column(
-                            children: [
-                              Text("Lives",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15),),
-                              Text("03",style: TextStyle(color: Colors.purple, fontWeight: FontWeight.bold, fontSize: 14),),
-                            ],
-                          )
-                        ],
+                child: Container(
+                  child: Column(
+                    children: [
+                      Image.asset("assets/images/man.png"),
+                      SizedBox(
+                        height: screenHeight / 60,
                       ),
-                    )
-                  ],
+                      Padding(
+                        padding: EdgeInsets.all(
+                          screenWidth / 40,
+                        ),
+                        child: Text(
+                          "Angel Sara",
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Center(
+                        child: Padding(
+                          padding: EdgeInsets.only(bottom: screenHeight / 60),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Column(
+                                children: [
+                                  Text(
+                                    "Balance",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: screenWidth / 25),
+                                  ),
+                                  Text(
+                                    "Rs.111",
+                                    style: TextStyle(
+                                        color: Colors.purple,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: screenWidth / 28),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                width: screenWidth / 6,
+                              ),
+                              Column(
+                                children: [
+                                  Text(
+                                    "Rank",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: screenWidth / 25),
+                                  ),
+                                  Text(
+                                    "10",
+                                    style: TextStyle(
+                                        color: Colors.purple,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: screenWidth / 28),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                width: screenWidth / 6,
+                              ),
+                              Column(
+                                children: [
+                                  Text(
+                                    "Lives",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: screenWidth / 25),
+                                  ),
+                                  Text(
+                                    "03",
+                                    style: TextStyle(
+                                        color: Colors.purple,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: screenWidth / 28),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
             Image.asset("assets/images/bg.png"),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => SoruSayfasi()),);
-              },
-              child: Text("Quiz'e Başla"),
-              style: ElevatedButton.styleFrom(backgroundColor: Colors.purple, padding: EdgeInsets.all(12) ,textStyle: TextStyle(fontSize: 20, wordSpacing: 5)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                DecoratedBox(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(colors: [
+                          Colors.purple,
+                          Colors.pink,
+                        ]),
+                        borderRadius: BorderRadius.circular(25),
+                        boxShadow: <BoxShadow>[
+                          BoxShadow(
+                              color: Color.fromRGBO(0, 0, 0, 0.57),
+                              blurRadius: 25),
+                        ]),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Center(
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.transparent,
+                                onSurface: Colors.transparent,
+                                shadowColor: Colors.transparent,
+                              ),
+                              onPressed: () {
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => SoruSayfasi()));
+                              },
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(
+                                    vertical: screenWidth / 25,
+                                    horizontal: screenHeight / 30),
+                                child: Text("Quiz Başlat"),
+                              )),
+                        ),
+                      ],
+                    ))
+              ],
             ),
           ],
         ),
@@ -95,34 +173,3 @@ class _StartPageState extends State<StartPage> {
     );
   }
 }
-
-/*child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Text(
-              "Basit Quiz Uygulaması",
-              style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  wordSpacing: 2,
-                  letterSpacing: 1),
-            ),
-            ElevatedButton(
-              child: Text("Soruları Görmek İçin Tıklayınız"),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SoruSayfasi(),
-                  ),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.grey,
-                  padding: EdgeInsets.all(15),
-                  textStyle:
-                  TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
-            ),
-          ],
-        ),*/
